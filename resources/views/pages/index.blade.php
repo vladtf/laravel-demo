0d15497cefbd36abd0ff13b72160291fb8586b8f
vladtf <vendeta0263@gmail.com>
2020-04-09 01:29:42 +0300

    Refactored by adding an app layout

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <title>{{config('app.name','LSAPP')}}</title>
 
-</head>
-<body>
-<h1>Welcome to Laravel</h1>
-<p>This is the Laravel application from "Laravel From Scratch" Youtube series</p>
-</body>
-</html>
+@section('content')
+    <h1>Welcome to Laravel</h1>
+    <p>This is the Laravel application from "Laravel From Scratch" Youtube series</p>
+@endsection
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <title>{{config('app.name','LSAPP')}}</title>
-
-</head>
-<body>
-<h1>Services</h1>
-<p>This is the services pages</p>
-</body>
-</html>
+@section('content')
+    <h1>Services</h1>
+    <p>This is the services pages</p>
+@endsection
