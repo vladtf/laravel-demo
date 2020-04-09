2072dbb3da2e8d74e50bf48a7e603bd2e273fac8
vladtf <vendeta0263@gmail.com>
2020-04-09 03:31:10 +0300

    Database migration

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,6 +5,7 @@
     <div class="jumbotron text-center">
         <h1>Welcome to Laravel</h1>
         <p>This is the Laravel application from "Laravel From Scratch" Youtube series</p>
-        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a><a class="btn btn-succes btn-lg" href="/register" role="button">Register</a>
+        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
+            <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
     </div>
 @endsection
