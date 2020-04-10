b8a316302a674e2207b51c8c8bda7c2c112542bb
vladtf <vendeta0263@gmail.com>
2020-04-10 16:10:37 +0300

    Add authentication

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,38 +1,36 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <link rel="script" href="{{asset('js/app.js')}}">
-    <link rel="script" href="{{asset('js/bootstrap.js')}}">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-            crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-            crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-            crossorigin="anonymous"></script>
 
-    <title>{{config('app.name','LSAPP')}}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
-@include('inc.navbar')
-<div class="container">
-    @include('inc.messages')
-    @yield('content')
-</div>
-
-<script src="//cdn.ckeditor.com/4.4.7/standard/ckeditor.js"></script>
-<script>
-    CKEDITOR.replace('article-ckeditor');
-</script>
+    <div id="app">
+        @include('inc.navbar')
+        <main class="py-4">
+            <div class="container">
+                @include('inc.messages')
+                @yield('content')
+            </div>
+        </main>
+    </div>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,7 @@ Route::get('/about',"PagesController@about");
 Route::get('/services',"PagesController@services");
 
 Route::resource('posts','PostController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
